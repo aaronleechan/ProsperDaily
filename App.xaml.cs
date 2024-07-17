22f13cf8bf5d63e3e4c847df5afe666429c36cac
aaron <aaron171091@gmail.com>
2024-07-16 17:07:02 -0700

    initial setup

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using MauiApp1;
+
+namespace ProsperDaily;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		//MainPage = new DashboardPage();
+		MainPage = new TransactionsPage();
+	}
+}
